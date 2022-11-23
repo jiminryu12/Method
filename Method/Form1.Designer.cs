d85948218b83857f3f25d418e9b7e74c64cf7a89
Jimin Ryu <Jimin Ryu@DESKTOP-CS29MPG>
2022-11-22 19:26:53 -0800

    Add project files.

---
+++ Method/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Method
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tboxResult = new System.Windows.Forms.TextBox();
+            this.tboxNum1 = new System.Windows.Forms.TextBox();
+            this.tboxNum2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Multiply = new System.Windows.Forms.Button();
+            this.Division = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tboxResult
+            // 
+            this.tboxResult.Enabled = false;
+            this.tboxResult.Location = new System.Drawing.Point(82, 75);
+            this.tboxResult.Name = "tboxResult";
+            this.tboxResult.Size = new System.Drawing.Size(148, 20);
+            this.tboxResult.TabIndex = 9;
+            // 
+            // tboxNum1
+            // 
+            this.tboxNum1.Location = new System.Drawing.Point(82, 157);
+            this.tboxNum1.Name = "tboxNum1";
+            this.tboxNum1.Size = new System.Drawing.Size(59, 20);
+            this.tboxNum1.TabIndex = 1;
+            // 
+            // tboxNum2
+            // 
+            this.tboxNum2.Location = new System.Drawing.Point(171, 157);
+            this.tboxNum2.Name = "tboxNum2";
+            this.tboxNum2.Size = new System.Drawing.Size(59, 20);
+            this.tboxNum2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(82, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(59, 47);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(82, 280);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(59, 47);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Multiply
+            // 
+            this.Multiply.Location = new System.Drawing.Point(171, 210);
+            this.Multiply.Name = "Multiply";
+            this.Multiply.Size = new System.Drawing.Size(59, 47);
+            this.Multiply.TabIndex = 5;
+            this.Multiply.Text = "x";
+            this.Multiply.UseVisualStyleBackColor = true;
+            this.Multiply.Click += new System.EventHandler(this.Multiply_Click);
+            // 
+            // Division
+            // 
+            this.Division.Location = new System.Drawing.Point(171, 280);
+            this.Division.Name = "Division";
+            this.Division.Size = new System.Drawing.Size(59, 47);
+            this.Division.TabIndex = 6;
+            this.Division.Text = "/";
+            this.Division.UseVisualStyleBackColor = true;
+            this.Division.Click += new System.EventHandler(this.Division_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(82, 350);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(148, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Clear";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(82, 400);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(148, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Exit";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "tboxResult";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "tboxNum1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(168, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "tboxNum2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.Division);
+            this.Controls.Add(this.Multiply);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tboxNum2);
+            this.Controls.Add(this.tboxNum1);
+            this.Controls.Add(this.tboxResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tboxResult;
+        private System.Windows.Forms.TextBox tboxNum1;
+        private System.Windows.Forms.TextBox tboxNum2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Multiply;
+        private System.Windows.Forms.Button Division;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Method/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Method
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //int iResult;
+            //iResult = Convert.ToInt32(tboxNum1.Text) + Convert.ToInt32(tboxNum2.Text);
+            int iA = Convert.ToInt32(tboxNum1.Text);
+            int iB = Convert.ToInt32(tboxNum2.Text);
+
+            tboxResult.Text = fPlus(iA, iB).ToString();
+        }
+
+        private int fPlus(int iA, int iB)
+        {
+            return iA + iB;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int iResult;
+            iResult = Convert.ToInt32(tboxNum1.Text) - Convert.ToInt32(tboxNum2.Text);
+            tboxResult.Text = Convert.ToString(iResult);
+        }
+
+        private void Multiply_Click(object sender, EventArgs e)
+        {
+            int iResult;
+            iResult = Convert.ToInt32(tboxNum1.Text) * Convert.ToInt32(tboxNum2.Text);
+            tboxResult.Text = Convert.ToString(iResult);
+        }
+
+        private void Division_Click(object sender, EventArgs e)
+        {
+            int iResult;
+            iResult = Convert.ToInt32(tboxNum1.Text) / Convert.ToInt32(tboxNum2.Text);
+            tboxResult.Text = Convert.ToString(iResult);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            tboxResult.Text = "";
+            tboxNum1.Text = "";
+            tboxNum2.Text = "";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
